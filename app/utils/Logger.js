9242ab34ba21b12a601d524e9916d14540d499fd
Gus <gusnitrous@gmail.com>
2018-05-21 22:54:38 +0400

    Fixed: exists path. Update packages.

--- app/utils/Logger.js
+++ app/utils/Logger.js
@@ -1,18 +1,27 @@
 const path = require('path');
+const fs = require('fs');
 const bunyan = require('bunyan');
 
-const logger = bunyan.createLogger({
+const dirLog = path.join(__dirname, '..', '..', 'logs');
+
+const options = {
     name: 'MySpend',
     time: (new Date).toLocaleString(),
     streams: [{
         type: 'rotating-file',
-        path: path.join(__dirname, '..', '..', 'logs', 'app-info.log'),
+        path: path.join(dirLog, 'app-info.log'),
     },
     {
         type: 'file',
-        path: path.join(__dirname, '..', '..', 'logs', 'app-error.log'),
+        path: path.join(dirLog, 'app-error.log'),
         level: 'error'
     }]
-});
+};
+
+if (!fs.existsSync(dirLog)) {
+    fs.mkdirSync(dirLog);
+}
+
+const logger = bunyan.createLogger(options);
 
 module.exports = logger;
