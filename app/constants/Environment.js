6c2b02802fa2ef65265b4b297d627e45560bd014
Gus <gusnitrous@gmail.com>
2018-05-22 22:35:08 +0400

    Fixed: using environment variables.

--- app/constants/Environment.js
+++ app/constants/Environment.js
@@ -3,15 +3,15 @@
  */
 
 /**
- * Приложение запущено в production режиме.
- * @type {boolean}
- */
-const PRODACTION_MODE = process.env.NODE_ENV === 'production';
-
-/**
  * Приложение запущено в develop режиме.
  * @type {boolean}
  */
-const DEVELOP_MODE = !PRODACTION_MODE;
+const DEVELOP_MODE = process.env.NODE_ENV === 'develop';
 
-module.exports = {PRODACTION_MODE, DEVELOP_MODE};
+/**
+ * Приложение запущено в production режиме.
+ * @type {boolean}
+ */
+const PRODUCTION_MODE = !DEVELOP_MODE;
+
+module.exports = {PRODUCTION_MODE, DEVELOP_MODE};
